ba1e1303991e4b86eb5ea62e39796983bdf2aa31
Cauã <cauabertolini@gmail.com>
2026-03-19 20:10:08 -0300

    Lista duplamente encadeada

--- AlgoritmosEEstruturaDeDados_2026/Lista05-ListasDuplamenteEncadeadas/src/ListaDupla.java
+++ AlgoritmosEEstruturaDeDados_2026/Lista05-ListasDuplamenteEncadeadas/src/ListaDupla.java
@@ -1,12 +1,10 @@
-public class ListaEncadeada<T> {
+public class ListaDupla<T> {
 
     private NoListaDupla primeiro;
 
-    public ListaEncadeada() {
+    public ListaDupla() {
         primeiro = new NoListaDupla<>();
-        primeiro.setProximo(null);
-        primeiro.setAnterior(null);
-
+        primeiro = null;
     }
 
     public NoListaDupla getPrimeiro() {
@@ -14,35 +12,42 @@ public class ListaEncadeada<T> {
     }
 
     public void inserir(T valor) {
-        NoListaDupla novoNo = new NoListaDupla();
+        NoListaDupla novoNo = new NoListaDupla<T>();
 
         novoNo.setInfo(valor);
-
         novoNo.setProximo(primeiro);
         novoNo.setAnterior(null);
 
-        if (primeiro != null) {
+        if (estaVazia()) {
             primeiro.setAnterior(novoNo);
         }
-        primeiro = novoNo;
+        this.primeiro = novoNo;
     }
 
     public void retirar(T valor) {
-        NoListaDupla<T> noAtual = primeiro;
-        NoListaDupla<T> noAnterior = null;
+        NoListaDupla noRemocao = buscar(valor);
 
-        while(noAtual.getProximo() != null && !noAtual.getInfo().equals(valor)) {
-            noAnterior = noAtual;
-            noAtual = noAtual.getProximo();
-        }
-
-        if (!noAtual.equals(null)) {
-            if (noAtual.equals(primeiro)) {
-                this.primeiro = primeiro.getProximo();
-            } else {
-                noAnterior.setProximo(noAtual.getProximo());
+        if (noRemocao != null) { // Existe?
+            if (primeiro == noRemocao) { // É o primeiro?
+                primeiro = noRemocao.getProximo(); // Se for o primeiro, o novo primeiro se torna o próximo dele e...
+            } else { // Não é o primeiro então o próximo do anterior recebe o próximo do atual
+                noRemocao.getAnterior().setProximo(noRemocao.getProximo()); //
             }
         }
+        if (noRemocao.getProximo() != null) { // Se não for o último...
+            noRemocao.getProximo().setAnterior(noRemocao.getAnterior()); // Então o anterior do próximo recebe o anterior dele
+        }
+    }
+
+    public void liberar() {
+        NoListaDupla noAtual = primeiro;
+        while (noAtual.getProximo() != null) {
+            NoListaDupla proximo = noAtual.getProximo();
+            noAtual.setAnterior(null);
+            noAtual.setProximo(null);
+            noAtual = proximo;
+        }
+        primeiro = null;
     }
 
     public void exibir() {
@@ -50,6 +55,14 @@ public class ListaEncadeada<T> {
         while (primeiro.getProximo() != null) {
             System.out.println(primeiro.getInfo());
             primeiro = primeiro.getProximo();
+        }
+    }
+
+    public void exibirOrdemInversa() {
+        NoListaDupla ultimo = obterNo(obterComprimento());
+        while (ultimo.getAnterior() != null) {
+            System.out.println(ultimo.getInfo());
+            ultimo = ultimo.getAnterior();
         }
     }
 
@@ -65,16 +78,6 @@ public class ListaEncadeada<T> {
             }
         }
         return null;
-    }
-
-    public int obterComprimento() {
-        int cont = -1;
-        NoListaDupla<T> no = getPrimeiro();
-        while (no.getProximo() != null) {
-            cont++;
-            no = no.getProximo();
-        }
-        return cont;
     }
 
     public NoListaDupla<T> obterNo(int idx) {
@@ -108,6 +111,16 @@ public class ListaEncadeada<T> {
         }
 
         return null;
+    }
+
+    public int obterComprimento() {
+        int cont = -1;
+        NoListaDupla<T> no = getPrimeiro();
+        while (no.getProximo() != null) {
+            cont++;
+            no = no.getProximo();
+        }
+        return cont;
     }
 
     @Override
--- AlgoritmosEEstruturaDeDados_2026/Lista05-ListasDuplamenteEncadeadas/src/main.java
+++ AlgoritmosEEstruturaDeDados_2026/Lista05-ListasDuplamenteEncadeadas/src/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        ListaEncadeada listaEncadeada = new ListaEncadeada<>();
+        ListaDupla listaDupla = new ListaDupla<>();
 
     }
 }
